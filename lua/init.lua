5fb72d1f401e2fb98ff4771b54fff4b650cafd98
Hunter Glass <h.rooster00@gmail.com>
2022-02-21 10:47:36 -0600

    Added rebinding for toggle explorer

--- lua/init.lua
+++ lua/init.lua
@@ -6,7 +6,9 @@ vim.opt.mouse = 'a'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.swapfile = false
-vim.cmd "colorscheme minischeme"
+vim.cmd("colorscheme minischeme")
+vim.cmd("nnoremap :e :NvimTreeToggle<CR>")
+vim.cmd(":set termguicolors")
 --vim.opt.foldmethod = "expr"
 --vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
