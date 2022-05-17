21583930d961a528eeedf3af8d208e5bb1247e9e
Hunter Glass <h.rooster00@gmail.com>
2022-05-17 00:38:17 -0500

    remove old option

--- lua/config.lua
+++ lua/config.lua
@@ -6,7 +6,6 @@ require('mini.pairs').setup()
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
   disable_netrw = true,
-  hide_root_folder = false,
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = true,
