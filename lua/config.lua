61c81af646e07907679b520b17849aefb46660d6
Hunter Glass <h.rooster00@gmail.com>
2022-09-26 13:27:33 -0500

    remove height option from nvim-tree

--- lua/config.lua
+++ lua/config.lua
@@ -18,7 +18,6 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   update_cwd = true,
   view = {
     width = 30,
-    height = 30,
     side = "left",
     preserve_window_proportions = false,
     number = false,
